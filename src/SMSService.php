11b4fffe034748cb088843782e1c327fe8a91e67
Birkan Esen <81517684+brknesn@users.noreply.github.com>
2023-03-16 18:47:40 +0300

    Update SMSService.php

--- src/SMSService.php
+++ src/SMSService.php
@@ -70,7 +70,14 @@ class SMSService
 
         foreach ($gateways as $recipients) {
             $gateway = $this->getGateway($recipients[0]);
-            $resp = $gateway->send($this->title, $this->message, $recipients, $this->type);
+            try{
+                $gateway->send($this->title, $this->message, $recipients, $this->type);
+            }catch (\Exception $e) {
+                $defaultGateway = config('laravel-multigw-sms-provider.default');
+                $gatewayConfig = $gateways[$defaultGateway];
+                $gateway = new $gatewayConfig['class']($gatewayConfig);
+                $gateway->send($this->title, $this->message, $recipients, $this->type);
+            }
         }
     }
 }
